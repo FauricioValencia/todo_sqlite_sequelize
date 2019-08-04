3ff0473ce63ba720a1ef863d54dd496fce58e5ca
Julian Fauricio Valencia Marin <jvalencia@reefstudios.co>
2019-08-04 02:01:06 -0500

    create models

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import express from "express";
+import consign from 'consign';
 
 const app = express();
-
+consign({
+  cwd:__dirname
+})
+.include('libs/config.js')
+.then('db.js')
+.then('libs/middlewares.js')
+.then('./routes')
+.then('libs/boot.js')
+.into(app)
 // Settings
-app.set("port", process.env.PORT || 3000);
 
-app.listen(app.get("port"), () => {
-  console.log("server on port", app.get("port"));
-});
+
